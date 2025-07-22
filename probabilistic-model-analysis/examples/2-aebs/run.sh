4a499e499cb82dde9fdf240ed4e208c94546f54b
irmanfaqrizal <irmanfaqrizal@gmail.com>
2025-07-22 10:11:36 +0200

    minor correction

--- probabilistic-model-analysis/examples/2-aebs/run.sh
+++ probabilistic-model-analysis/examples/2-aebs/run.sh
@@ -1,21 +1,21 @@
 confnum=2
 confIdx=1
 
-# for (( c=1; c<=$confnum; c++ ))
-# do
-#     printf  "\n!!!!! Generating C$c LTS !!!!!\n"
-#     if [ $c == 1 ]
-#     then
-#         cd configurations/C$c/
-#     else
-#         cd ../C$c/
-#     fi
-#     ./build.sh
-# done
+for (( c=1; c<=$confnum; c++ ))
+do
+    printf  "\n!!!!! Generating C$c LTS !!!!!\n"
+    if [ $c == 1 ]
+    then
+        cd configurations/C$c/
+    else
+        cd ../C$c/
+    fi
+    ./build.sh
+done
 
-# printf  "\n!!!!! Running the simulator !!!!!\n"
-# cd ../..
-# python simulator.py
+printf  "\n!!!!! Running the simulator !!!!!\n"
+cd ../..
+python simulator.py
 
 confIdx=1
 for (( c=1; c<=$confnum; c++ ))
@@ -33,6 +33,6 @@ do
     ./query.sh
 done
 
-# printf  "\n!!!!! Plotting the graphs !!!!!\n"
-# cd ../..
-# python graphall.py
+printf  "\n!!!!! Plotting the graphs !!!!!\n"
+cd ../..
+python graphall.py
